6f765cd462fb1f36ae17b7a1b311ba2f150aa82e
Andrei Surgai <94757464+spo9n@users.noreply.github.com>
2021-12-11 11:48:29 +0200

    Add files via upload

---
+++ PR2_Surgai_ZVD1/PR2_Surgai_ZVD1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace PR2_Surgai_ZVD1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.sortButton = new System.Windows.Forms.Button();
+            this.longestSurnameButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(293, 356);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(12, 396);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 40);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Додати";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // sortButton
+            // 
+            this.sortButton.Location = new System.Drawing.Point(123, 395);
+            this.sortButton.Name = "sortButton";
+            this.sortButton.Size = new System.Drawing.Size(75, 41);
+            this.sortButton.TabIndex = 2;
+            this.sortButton.Text = "Сортувати";
+            this.sortButton.UseVisualStyleBackColor = true;
+            this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
+            // 
+            // longestSurnameButton
+            // 
+            this.longestSurnameButton.Location = new System.Drawing.Point(230, 395);
+            this.longestSurnameButton.Name = "longestSurnameButton";
+            this.longestSurnameButton.Size = new System.Drawing.Size(75, 41);
+            this.longestSurnameButton.TabIndex = 3;
+            this.longestSurnameButton.Text = "Найдовше прізвище";
+            this.longestSurnameButton.UseVisualStyleBackColor = true;
+            this.longestSurnameButton.Click += new System.EventHandler(this.longestSurnameButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 450);
+            this.Controls.Add(this.longestSurnameButton);
+            this.Controls.Add(this.sortButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.richTextBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сургай Андрій П-81 ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button sortButton;
+        private System.Windows.Forms.Button longestSurnameButton;
+    }
+}
+
---
+++ PR2_Surgai_ZVD1/PR2_Surgai_ZVD1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR2_Surgai_ZVD1
+{
+    public partial class Form1 : Form
+    {
+        List<string> surnameList;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            surnameList = new List<string>();
+
+            foreach (string str in richTextBox1.Lines)
+            {
+                surnameList.Add(str);
+            }
+
+            MessageBox.Show("Прізвища було додано у масив рядків.", "Успіх!");
+        }
+
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+            surnameList.Sort();
+
+            richTextBox1.Text = "";
+
+            foreach(string str in surnameList)
+            {
+                richTextBox1.Text += str + "\n";
+            }
+        }
+
+        private void longestSurnameButton_Click(object sender, EventArgs e)
+        {
+            string longestSurname = surnameList[0];
+
+            for (int i = 0; i < surnameList.Count(); i++)
+            {
+                if (surnameList[i].Length > longestSurname.Length)
+                {
+                    longestSurname = surnameList[i];
+                }
+            }
+
+            MessageBox.Show("Найдовше прізвище - " + longestSurname.ToString(), "Пошук найдовшого прізвища...");
+        }
+
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((char)e.KeyChar == (Char)Keys.Back) return;
+            if (char.IsLetter(e.KeyChar)) return;
+            e.Handled = true;
+        }
+    }
+}
